2bf26ed0651bbf2ead31d9633ae0a16c8e023b07
Daniel <dnomura@twiagemed.com>
2019-12-15 13:59:09 -0800

    Resolved type/build errors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,55 @@
 import Vue from "vue";
 import App from "./App.vue";
+import { newEmployee } from "./graphql/subscriptions";
+
+import aws_exports from "./aws-exports";
+import AWSAppSyncClient from "aws-appsync";
+import AWS from "aws-sdk";
+import gql from "graphql-tag";
+import { AUTH_TYPE } from "aws-appsync/lib";
+import { AuthOptions } from "aws-appsync-auth-link/lib/auth-link";
 
 Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App)
 }).$mount("#app");
+
+const url = aws_exports.aws_appsync_graphqlEndpoint;
+const region = aws_exports.aws_project_region;
+
+AWS.config.update({
+  region,
+  credentials: new AWS.Credentials({
+    accessKeyId: process.env.VUE_APP_AWS_ACCESS_KEY_ID || "",
+    secretAccessKey: process.env.VUE_APP_AWS_SECRET_ACCESS_KEY || ""
+  })
+});
+const credentials = AWS.config.credentials;
+
+const auth = {
+  type: AUTH_TYPE.AWS_IAM,
+  credentials
+} as AuthOptions;
+
+const client = new AWSAppSyncClient({
+  url,
+  region,
+  auth
+});
+
+const subquery = gql(newEmployee);
+
+const test = async () => {
+  await client.hydrated();
+  const observable = client.subscribe({ query: subquery });
+  observable.subscribe({
+    next: function(data: any) {
+      console.log("realtime data: ", data);
+    },
+    complete: console.log,
+    error: console.error
+  });
+};
+
+test();
